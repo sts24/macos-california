b8f177a2b91ea74686b70c3c2059931b565077d4
Scott Smith <stsmith@callutheran.edu>
2020-07-08 15:50:10 -0700

    placeholder for now

---
+++ components/Map.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="map-area">
+
+	  {{ location }}
+
+  </section>
+</template>
+
+<script>
+export default {
+	props: ['location']
+}
+</script>
+
+<style>
+	.map-area {
+		order: 1;
+		background: gray;
+	}
+</style>
