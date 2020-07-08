408d623df90ff22d1cdd256b5c083cdb03557022
Scott Smith <stsmith@callutheran.edu>
2020-07-08 15:50:04 -0700

    nav component that takes in props

---
+++ components/Navigation.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="app-nav">
+
+	  <ul>
+		  <li v-for="item of NavData" :key="item.slug">
+			  <nuxt-link :to="item.slug">{{ item.title }}</nuxt-link>
+		  </li>
+	  </ul>
+
+  </nav>
+</template>
+
+<script>
+export default {
+	props: ['NavData']
+}
+</script>
+
+<style>
+	.app-nav {
+		padding: 1rem;
+	}
+
+	.app-nav > ul {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.app-nav > ul > li {
+		margin: 0;
+		padding: 0;
+	}
+	
+</style>
