a161a5511607da146fb4933a7b4565b8c2dd9156
Scott Smith <stsmith@callutheran.edu>
2020-07-09 14:24:50 -0700

    highlight selected page

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,11 +1,14 @@
 <template>
   <nav class="app-nav">
 
-	  <ul>
-		  <li v-for="item of NavData" :key="item.slug">
-			  <nuxt-link :to="item.slug">{{ item.title }}</nuxt-link>
-		  </li>
-	  </ul>
+	<ul>
+		<li v-for="item of NavData" :key="item.slug">
+			<nuxt-link :to="item.slug">
+				<span class="link-number">{{ item.version }}</span>
+				<span class="link-label">{{ item.title }}</span>
+			</nuxt-link>
+		</li>
+	</ul>
 
   </nav>
 </template>
@@ -34,4 +37,32 @@ export default {
 		padding: 0;
 	}
 	
+	.app-nav > ul > li > a {
+		display: block;
+		text-decoration: none;
+	}
+
+	.app-nav > ul > li > a.nuxt-link-active .link-number {
+		outline: 3px solid blue;
+	}
+
+	.link-number {
+		display: flex;
+		width: 100px;
+		height: 100px;
+		background-color: gray;
+		justify-content: center;
+		align-items: center;
+		color: black;
+		font-size: 1.5em;
+	}
+
+	.link-label {
+		display: block;
+		margin-top: 0.5em;
+		text-align: center;
+		font-weight: bold;
+		color: black;
+	}
+
 </style>
