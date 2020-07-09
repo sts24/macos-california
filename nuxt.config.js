f4f43d11e79eaa1cac7b04dc2776c7a9af5b61b8
Scott Smith <stsmith@callutheran.edu>
2020-07-09 14:13:52 -0700

    added cloudinary plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
   ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
+    '@/plugins/cloudinary.js'
   ],
   /*
   ** Auto import components
---
+++ plugins/cloudinary.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+import Cloudinary from "cloudinary-vue";
+
+
+Vue.use(Cloudinary);
